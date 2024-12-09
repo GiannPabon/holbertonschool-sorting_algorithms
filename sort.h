2237c7bdd34eab252efcd33c95d31761bb8bc0a6
Giann Pabon <giannalberto2@outlook.com>
2024-12-09 15:59:52 -0400

    Create sort.h file

---
+++ sort.h
@@ -0,0 +1,21 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+
+print_array;
+print_list;
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+
+
+
+
+#endif
