a6f57451912d030278908c5e1b53840cfe0f23c2
Giann Pabon <giannalberto2@outlook.com>
2024-12-12 14:10:46 -0400

    Update sort.h to fix listint_t error

--- sort.h
+++ sort.h
@@ -1,18 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-
-
-print_array;
-print_list;
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
+#include <stdio.h>   /* For printf in print_array and print_list */
+#include <stdlib.h>  /* For malloc and free */
+#include <stddef.h>  /* For size_t */
 
 /**
  * struct listint_s - Doubly linked list node
@@ -23,11 +14,19 @@ void quick_sort(int *array, size_t size);
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
+/* Function prototypes */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
-#endif
+#endif /* SORT_H */
+
