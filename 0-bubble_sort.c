dbbe79acd11d9ce18256d94f4b5fa548205eac8b
Giann Pabon <giannalberto2@outlook.com>
2024-12-12 14:18:43 -0400

    fix betty characters error in bubble sort file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 #include <stdio.h>
 /**
- * bubble_sort - Sorts an array of integers in ascending order using Bubble Sort.
+ * bubble_sort - Sorts an array of int's in ascending order using Bubble Sort.
  * @array: Pointer to the array to be sorted.
  * @size: Number of elements in the array.
  */
@@ -10,6 +10,7 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 	int swapped;
+
 	if (array == NULL || size < 2)
 		return;
 	for (i = 0; i < size - 1; i++)
